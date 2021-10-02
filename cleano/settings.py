0c9d9b85057fb87750ad970098bd04669197ed2b
Diyar <djars500@gmail.com>
2021-10-02 18:47:17 +0600

    third

--- cleano/settings.py
+++ cleano/settings.py
@@ -93,7 +93,7 @@ else:
    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbforproject',
+        'NAME': 'cleano_data',
         'USER': 'projectdbuser',
         'PASSWORD': 'password',
         'HOST': 'localhost',
